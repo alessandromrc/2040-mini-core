7433d13a37c748f20fb1ea5cf96dc6d48d2e7d06
Alessandro <66976091+alessandromrc@users.noreply.github.com>
2022-11-22 00:47:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ Midi midi;
 System system;
 USB_SERIAL usb_serial;
 Temperature temp;
+Core core;
+Interrupt interrupt;
 
 bool led_status = false;
 
@@ -13,57 +15,79 @@ unsigned long interval = 50;
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 const uint switch_reset = 2;
 
-
 Debouncing Reset(switch_reset, INPUT_PULLUP);
 
 
-void setup() {
-  initializeADC();
-  temp.Begin();
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(26, ANALOG);
-  midi.Begin(uart0, 0, 1);
-}
-
-void loop() {
-
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    led_status = !led_status;
-    digitalWrite(LED_PIN, led_status);
-    previousMillis = currentMillis;
-  }
-
-  midi.sendNoteOn(60, 127);
-  delay(10);
-  midi.sendNoteOff(60, 127);
-  delay(10);
-
-  /*
-  usb_serial.println(analogRead(26));
-
-  usb_serial.print("temperature -> ");
-
-  usb_serial.print(temp.read('C'));
-  usb_serial.println("C");
-  */
-
-
-  const char* character_incoming = usb_serial.getString();
-
-  usb_serial.print(character_incoming);
-}
-
-int main() {
-  stdio_init_all();
-  setup();
-  while (true) {
-    if (Reset.press()) {
-      system.reset();
+void loop1() {
+    while(true)
+    {
+        digitalWrite(LED_PIN, HIGH);
+        delay(100);
+        digitalWrite(LED_PIN, LOW);
+        delay(100);
+        //usb_serial.println(system.arithmetic_shift_right(100, 5));
     }
-    loop();
-  }
-  return 0;
+}
+
+
+const uint EncoderPin[] = {3, 4};
+
+
+void gpio_callback(uint gpio, uint32_t events);
+
+
+Encoder encoder(EncoderPin, gpio_callback);
+
+
+void gpio_callback(uint gpio, uint32_t events)
+{
+    encoder.update(gpio);
+}
+
+void setup()
+{
+    initializeADC();
+    temp.Begin();
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(16, OUTPUT);
+    pinMode(26, ANALOG);
+    midi.Begin(uart0, 0, 1);
+    core.launch_task(loop1);
+}
+
+
+
+
+
+
+void loop()
+{
+    /*
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval) {
+        led_status = !led_status;
+        digitalWrite(LED_PIN, led_status);
+        previousMillis = currentMillis;
+    }
+    */
+    //usb_serial.println(clock_get_hz(clk_sys));
+
+    writeFrequency(21, system.abs(encoder.getValue()));
+
+    usb_serial.println(system.abs(encoder.getValue()));
+}
+
+int main()
+{
+    stdio_init_all();
+    setup();
+    while (true) {
+        if (Reset.press()) {
+            system.reset();
+        }
+        loop();
+        tight_loop_contents();
+    }
+    return 0;
 }
